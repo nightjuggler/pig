382d304a3f8a4a1d2a396074836ed6e951245800
Pius Fischer <pius@nightjuggler.com>
2016-03-15 08:22:38 -0700

    Declare imageNames with var, use .format() instead of %, remove tab before closing bracket

--- cropList.py
+++ cropList.py
@@ -3,8 +3,8 @@ import os
 
 if __name__ == '__main__':
 	js_file = open('cropList.js', 'w')
-	js_file.write('imageNames = [\n')
-	js_file.write(',\n'.join(['\t"%s"' % name for name in os.listdir('originals')
+	js_file.write('var imageNames = [\n')
+	js_file.write(',\n'.join(['\t"{}"'.format(name) for name in os.listdir('originals')
 		if name[-4:] in ('.JPG', '.jpg', '.PNG', '.png')]))
-	js_file.write('\n\t];\n')
+	js_file.write('\n];\n')
 	js_file.close()
