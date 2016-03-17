3fd6821612ff0cee50dfe6e3df993d184ab8f481
Pius Fischer <pius@nightjuggler.com>
2016-03-17 11:10:10 -0700

    Don't show the crop geometry div by default. Toggle display of crop geometry div by pressing 'g'. Better styling of the Copy links in the filter code div. Simplify selection code by using Selection.selectAllChildren() (This also prevents ugly-ish Safari behavior of selecting cssCode's bottom margin)

--- cropList.py
+++ cropList.py
@@ -3,6 +3,7 @@ import os
 
 if __name__ == '__main__':
 	js_file = open('cropList.js', 'w')
+	js_file.write('var showCropGeometry = true;\n')
 	js_file.write('var imageNames = [\n')
 	js_file.write(',\n'.join(['\t"{}"'.format(name) for name in os.listdir('originals')
 		if name[-4:] in ('.JPG', '.jpg', '.PNG', '.png')]))
