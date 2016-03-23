99ae6a53ead57b4495b09ac3dfdb81620bf8e810
Pius Fischer <pius@nightjuggler.com>
2016-03-22 22:56:28 -0700

    Added polarTransform(), applyDBlurX/Y(), and applyThreshold(). Added support for edgeMode="wrap" (aka "tile") in boxBlur().

--- filter.js
+++ filter.js
@@ -175,7 +175,11 @@ function createBlurFilter(radius, channels)
 {
 	return createFilter(channels, createGaussianBlur(radius, radius));
 }
-function boxBlur(blurInfo, radiusType, vertical)
+var BlurFlags = {
+	xDirectional: 0b00000001,
+	yDirectional: 0b00000010,
+};
+function boxBlur(blurInfo, radiusType)
 {
 	var leftRadius = blurInfo[radiusType + 'Left'];
 	var rightRadius = blurInfo[radiusType + 'Right'];
@@ -196,12 +200,11 @@ function boxBlur(blurInfo, radiusType, vertical)
 	var delta = 4;
 	var vDelta = delta * width;
 
-	if (vertical) {
+	if (radiusType.charAt(0) === 'y') {
 		[width, height] = [height, width]; // Swap width and height
 		[delta, vDelta] = [vDelta, delta]; // Swap delta and vDelta
 	}
 
-	var m = leftRadius + 1;
 	var zDelta = (width - 1) * delta;
 	vDelta -= zDelta + delta;
 	width -= size;
@@ -212,45 +215,77 @@ function boxBlur(blurInfo, radiusType, vertical)
 	var blurB = blurInfo.blurB;
 	var blurA = blurInfo.blurA;
 
+	var edge = src;
+
+	var eFirst      = 0;
+	var eLast       = 0;
+	var eFirstStart = 0;
+	var eLastStart  = 0;
+	var eDelta      = 0;
+
+	if (edgeMode === 0) // "black" or "none"
+	{
+		edge = [0, 0, 0, 0];
+	}
+	else if (edgeMode === 1) // "tile" or "wrap"
+	{
+		eFirstStart = zDelta - (leftRadius - 1) * delta;
+		eDelta = delta;
+	}
+	else if (edgeMode === 2) // "edge" or "duplicate"
+	{
+		eLastStart = zDelta;
+	}
+	else if (edgeMode === 3) // "mirror"
+	{
+		eFirstStart = (leftRadius - 1) * delta;
+		eLastStart = zDelta;
+		eDelta = -delta;
+	}
+
 	var i = 0;
 	for (var y = 0; y < height; ++y) {
 		var j = i;
 		var k = i;
 
-		var firstR, sumR, lastR;
-		var firstG, sumG, lastG;
-		var firstB, sumB, lastB;
-		var firstA, sumA, lastA;
+		var sumR = 0;
+		var sumG = 0;
+		var sumB = 0;
+		var sumA = 0;
 
-		if (edgeMode === 0) {
-			// edgeMode is "none"
-			firstR = lastR = 0; sumR = firstR * m;
-			firstG = lastG = 0; sumG = firstG * m;
-			firstB = lastB = 0; sumB = firstB * m;
-			firstA = lastA = 0; sumA = firstA * m;
-		} else {
-			// edgeMode is "duplicate"
-			var z = i + zDelta;
-			firstR = src[i  ]; sumR = firstR * m; lastR = src[z  ];
-			firstG = src[i+1]; sumG = firstG * m; lastG = src[z+1];
-			firstB = src[i+2]; sumB = firstB * m; lastB = src[z+2];
-			firstA = src[i+3]; sumA = firstA * m; lastA = src[z+3];
+		if (edgeMode !== 0) {
+			eFirst = i + eFirstStart;
+			eLast = i + eLastStart;
 		}
-
-		for (var x = 0; x < rightRadius; ++x) {
+		var eFirstSaved = eFirst;
+		for (var x = 0; x < leftRadius; ++x) {
+			sumR += edge[eFirst  ];
+			sumG += edge[eFirst+1];
+			sumB += edge[eFirst+2];
+			sumA += edge[eFirst+3];
+			eFirst += eDelta;
+		}
+		eFirst = eFirstSaved;
+		for (var x = 0; x <= rightRadius; ++x) {
 			sumR += src[k  ];
 			sumG += src[k+1];
 			sumB += src[k+2];
 			sumA += src[k+3];
 			k += delta;
 		}
-		for (var x = 0; x <= leftRadius; ++x) {
-			if (blurR) { sumR += src[k  ] - firstR; d[i  ] = sumR / size; }
-			if (blurG) { sumG += src[k+1] - firstG; d[i+1] = sumG / size; }
-			if (blurB) { sumB += src[k+2] - firstB; d[i+2] = sumB / size; }
-			if (blurA) { sumA += src[k+3] - firstA; d[i+3] = sumA / size; }
+		if (blurR) d[i  ] = sumR / size;
+		if (blurG) d[i+1] = sumG / size;
+		if (blurB) d[i+2] = sumB / size;
+		if (blurA) d[i+3] = sumA / size;
+		i += delta;
+		for (var x = 0; x < leftRadius; ++x) {
+			if (blurR) { sumR += src[k  ] - edge[eFirst  ]; d[i  ] = sumR / size; }
+			if (blurG) { sumG += src[k+1] - edge[eFirst+1]; d[i+1] = sumG / size; }
+			if (blurB) { sumB += src[k+2] - edge[eFirst+2]; d[i+2] = sumB / size; }
+			if (blurA) { sumA += src[k+3] - edge[eFirst+3]; d[i+3] = sumA / size; }
 			k += delta;
 			i += delta;
+			eFirst += eDelta;
 		}
 		for (var x = width; x > 0; --x) {
 			if (blurR) { sumR += src[k  ] - src[j  ]; d[i  ] = sumR / size; }
@@ -262,12 +297,13 @@ function boxBlur(blurInfo, radiusType, vertical)
 			i += delta;
 		}
 		for (var x = 0; x < rightRadius; ++x) {
-			if (blurR) { sumR += lastR - src[j  ]; d[i  ] = sumR / size; }
-			if (blurG) { sumG += lastG - src[j+1]; d[i+1] = sumG / size; }
-			if (blurB) { sumB += lastB - src[j+2]; d[i+2] = sumB / size; }
-			if (blurA) { sumA += lastA - src[j+3]; d[i+3] = sumA / size; }
+			if (blurR) { sumR += edge[eLast  ] - src[j  ]; d[i  ] = sumR / size; }
+			if (blurG) { sumG += edge[eLast+1] - src[j+1]; d[i+1] = sumG / size; }
+			if (blurB) { sumB += edge[eLast+2] - src[j+2]; d[i+2] = sumB / size; }
+			if (blurA) { sumA += edge[eLast+3] - src[j+3]; d[i+3] = sumA / size; }
 			j += delta;
 			i += delta;
+			eLast += eDelta;
 		}
 		i += vDelta;
 	}
@@ -346,6 +382,31 @@ function getBoxBlurSizes2(blurInfo, radiusType)
 		blurInfo[radiusType + i + 'Right'] = radius + 1;
 	}
 }
+function getBoxBlurSizes(blurInfo, radiusType)
+{
+	var directional = (blurInfo.flags & BlurFlags[radiusType + 'Directional']) !== 0;
+
+	radiusType += 'Radius';
+
+	if (!directional) {
+		getBoxBlurSizes1(blurInfo, radiusType);
+		return;
+	}
+
+	var radius = blurInfo[radiusType];
+	var radiusLeft = 0;
+	var radiusRight = 0;
+
+	if (radius < 0)
+		radiusLeft = -radius;
+	else
+		radiusRight = radius;
+
+	for (var i = 1; i <= 3; ++i) {
+		blurInfo[radiusType + i + 'Left'] = radiusLeft;
+		blurInfo[radiusType + i + 'Right'] = radiusRight;
+	}
+}
 function copyUnchangedData(blurInfo)
 {
 	var copyR = !blurInfo.blurR;
@@ -366,24 +427,24 @@ function copyUnchangedData(blurInfo)
 }
 function blur(blurInfo)
 {
-	getBoxBlurSizes1(blurInfo, 'xRadius');
-	getBoxBlurSizes1(blurInfo, 'yRadius');
+	getBoxBlurSizes(blurInfo, 'x');
+	getBoxBlurSizes(blurInfo, 'y');
 
-	boxBlur(blurInfo, 'xRadius1', false);
-	boxBlur(blurInfo, 'yRadius1', true);
+	boxBlur(blurInfo, 'xRadius1');
+	boxBlur(blurInfo, 'yRadius1');
 
-	boxBlur(blurInfo, 'xRadius2', false);
-	boxBlur(blurInfo, 'yRadius2', true);
+	boxBlur(blurInfo, 'xRadius2');
+	boxBlur(blurInfo, 'yRadius2');
 
-	boxBlur(blurInfo, 'xRadius3', false);
-	boxBlur(blurInfo, 'yRadius3', true);
+	boxBlur(blurInfo, 'xRadius3');
+	boxBlur(blurInfo, 'yRadius3');
 
 	if (!blurInfo.blurAll && blurInfo.inData === blurInfo.newData)
 		copyUnchangedData(blurInfo);
 
 	return blurInfo.inData;
 }
-function BlurInfo(context, inData, xRadius, yRadius, channels, edgeMode)
+function BlurInfo(context, inData, xRadius, yRadius, channels, edgeMode, flags)
 {
 	this.inData = inData;
 	this.outData = context.createImageData(inData);
@@ -391,7 +452,17 @@ function BlurInfo(context, inData, xRadius, yRadius, channels, edgeMode)
 
 	this.xRadius = xRadius;
 	this.yRadius = yRadius;
-	this.edgeMode = edgeMode;
+	this.flags = flags;
+
+	if (edgeMode === 'black' || edgeMode === 'none')
+		this.edgeMode = 0;
+	else if (edgeMode === 'tile' || edgeMode === 'wrap')
+		this.edgeMode = 1;
+	else if (edgeMode === 'mirror')
+		this.edgeMode = 3;
+	else // (edgeMode === 'edge' || edgeMode === 'duplicate')
+		this.edgeMode = 2;
+
 	this.blurR = false;
 	this.blurG = false;
 	this.blurB = false;
@@ -418,21 +489,168 @@ function BlurInfo(context, inData, xRadius, yRadius, channels, edgeMode)
 
 	this.blurAll = this.blurR && this.blurG && this.blurB && this.blurA;
 }
-function applyBlur(context, imageData, xRadius, yRadius, channels, edgeMode)
+function applyBlur(context, imageData, xRadius, yRadius, channels, edgeMode, flags)
 {
-	var blurInfo = new BlurInfo(context, imageData, xRadius, yRadius, channels, edgeMode);
+	var blurInfo = new BlurInfo(context, imageData, xRadius, yRadius, channels, edgeMode, flags);
 
 	return blur(blurInfo);
 }
 function applyBlurFilter(context, imageData, radius, channels)
 {
-	return applyBlur(context, imageData, radius, radius, channels, 1);
+	return applyBlur(context, imageData, radius, radius, channels, 'edge', 0);
 }
 function applyBlurXFilter(context, imageData, radius, channels)
 {
-	return applyBlur(context, imageData, radius, 0, channels, 1);
+	return applyBlur(context, imageData, radius, 0, channels, 'edge', 0);
 }
 function applyBlurYFilter(context, imageData, radius, channels)
 {
-	return applyBlur(context, imageData, 0, radius, channels, 1);
+	return applyBlur(context, imageData, 0, radius, channels, 'edge', 0);
+}
+function applyDBlurX(context, imageData, radius, channels)
+{
+	return applyBlur(context, imageData, radius, 0, channels, 'edge', BlurFlags.xDirectional);
+}
+function applyDBlurY(context, imageData, radius, channels)
+{
+	return applyBlur(context, imageData, 0, radius, channels, 'edge', BlurFlags.yDirectional);
+}
+var Polar = {
+	RadiusHalfDiagonal: function(w,h) {return Math.sqrt(w*w + h*h)/2;},
+	RadiusHalfWidth:    function(w,h) {return w/2;},
+	RadiusHalfHeight:   function(w,h) {return h/2;},
+	RadiusHeight:       function(w,h) {return h;},
+	EdgeBlack:          new Uint8ClampedArray([0, 0, 0, 255]),
+	EdgeTransparent:    new Uint8ClampedArray([0, 0, 0, 0]),
+	EdgeDuplicate:      null,
+};
+function setChannelFlags(info, channels)
+{
+	info.setR = false;
+	info.setG = false;
+	info.setB = false;
+	info.setA = false;
+
+	if (typeof channels === 'number') {
+		if ((channels & 1) === 1) info.setR = true;
+		if ((channels & 2) === 2) info.setG = true;
+		if ((channels & 4) === 4) info.setB = true;
+		if ((channels & 8) === 8) info.setA = true;
+	} else if (typeof channels === 'string') {
+		for (var channel of channels)
+			switch (channel) {
+				case 'A': info.setA = true; break;
+				case 'B': info.setB = true; break;
+				case 'G': info.setG = true; break;
+				case 'R': info.setR = true; break;
+			}
+	} else if (typeof channels === 'undefined') {
+		info.setR = true;
+		info.setG = true;
+		info.setB = true;
+		info.setA = true;
+	}
+}
+function polarTransform(context, inData, channels, info)
+{
+	if (info === undefined)
+		info = {};
+
+	var width = inData.width;
+	var height = inData.height;
+
+	var reverse = (typeof info.reverse === 'boolean') ? info.reverse : false;
+	var radius = ((typeof info.radius === 'function') ? info.radius : Polar.RadiusHalfDiagonal)(width, height);
+	var radiusScale = radius / height;
+
+	var edgePixel = (info.edgePixel instanceof Uint8ClampedArray
+		&& info.edgePixel.length === 4) ? info.edgePixel : null;
+
+	// Multiply degrees by pi/180 to convert to radians.
+	// Note that when doing a reverse polar transform, Math.atan2() will always return an angle
+	// between -pi and pi (-180 and 180 degrees). Thus minAngle should be >= -180, and maxAngle
+	// should be <= 180.
+
+	var minAngle = (typeof info.minAngle === 'number' ? info.minAngle : -180) * Math.PI/180;
+	var maxAngle = (typeof info.maxAngle === 'number' ? info.maxAngle :  180) * Math.PI/180;
+	var angleScale = (maxAngle - minAngle) / (width - 1);
+
+	var centerX = Math.round(width / 2);
+	var centerY = Math.round(height / 2);
+
+	var outData = context.createImageData(inData);
+	var s = inData.data;
+	var d = outData.data;
+
+	setChannelFlags(info, channels);
+	var setR = info.setR;
+	var setG = info.setG;
+	var setB = info.setB;
+	var setA = info.setA;
+
+	var x, y, hypot, angle;
+
+	for (var j = 0; j < height; ++j)
+		for (var i = 0; i < width; ++i)
+		{
+			if (reverse) {
+				var deltaY = j - centerY;
+				var deltaX = i - centerX;
+
+				hypot = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+				angle = Math.atan2(deltaY, deltaX);
+
+				y = Math.round(hypot / radiusScale);
+				x = Math.round((angle - minAngle) / angleScale);
+			} else {
+				hypot = j * radiusScale;
+				angle = minAngle + i * angleScale;
+
+				x = centerX + Math.round(hypot * Math.cos(angle));
+				y = centerY + Math.round(hypot * Math.sin(angle));
+			}
+
+			var di = (j * width + i) * 4;
+
+			var edge = false;
+			if (x < 0) {
+				edge = true; x = 0;
+			} else if (x >= width) {
+				edge = true; x = width - 1;
+			}
+			if (y < 0) {
+				edge = true; y = 0;
+			} else if (y >= height) {
+				edge = true; y = height - 1;
+			}
+
+			if (edge && edgePixel !== null) {
+				d[di  ] = setR ? edgePixel[0] : s[di  ];
+				d[di+1] = setG ? edgePixel[1] : s[di+1];
+				d[di+2] = setB ? edgePixel[2] : s[di+2];
+				d[di+3] = setA ? edgePixel[3] : s[di+3];
+			} else {
+				var si = (y * width + x) * 4;
+				d[di  ] = setR ? s[si  ] : s[di  ];
+				d[di+1] = setG ? s[si+1] : s[di+1];
+				d[di+2] = setB ? s[si+2] : s[di+2];
+				d[di+3] = setA ? s[si+3] : s[di+3];
+			}
+		}
+
+	return outData;
+}
+function applyThreshold(d, amount, channels)
+{
+	var setR = ((channels & 1) === 1);
+	var setG = ((channels & 2) === 2);
+	var setB = ((channels & 4) === 4);
+
+	var dLen = d.length;
+	for (var i = 0; i < dLen; i += 4)
+	{
+		if (setR) d[i]     = (d[i    ] < amount ? 0 : 255);
+		if (setG) d[i + 1] = (d[i + 1] < amount ? 0 : 255);
+		if (setB) d[i + 2] = (d[i + 2] < amount ? 0 : 255);
+	}
 }
