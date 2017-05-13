1ae6b58f5ca77a59ad7063e53a0558fd9a4b086d
Pius Fischer <pius@nightjuggler.com>
2017-05-12 22:19:53 -0700

    The originals directory can now be specified on the command line. For example: ./cropList.py originals_iphone

--- cropList.py
+++ cropList.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 import os
 
-if __name__ == '__main__':
+def writeCropList(imageDir):
 	js_file = open('cropList.js', 'w')
 	js_file.write('var showCropGeometry = true;\n')
-	js_file.write('var imagePath = "originals/";\n')
+	js_file.write('var imagePath = "{}/";\n'.format(imageDir))
 	js_file.write('var imageNames = [\n')
-	js_file.write(',\n'.join(['\t"{}"'.format(name) for name in os.listdir('originals')
+	js_file.write(',\n'.join(['\t"{}"'.format(name) for name in os.listdir(imageDir)
 		if name[-4:] in ('.JPG', '.jpg', '.PNG', '.png')]))
 	js_file.write('\n];\n')
 	js_file.close()
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('imageDir', nargs='?', default='originals')
+	args = parser.parse_args()
+
+	writeCropList(args.imageDir)
