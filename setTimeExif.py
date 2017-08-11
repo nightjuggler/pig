caafebfe542651c00f838191ead905373181ab66
Pius Fischer <pius@nightjuggler.com>
2017-08-11 15:05:36 -0700

    Utility for setting a JPEG file's modification (and access) time to the date and time specified in the Exif DateTimeOriginal tag.

---
+++ setTimeExif.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import os
+import time
+from pimly import Image
+
+def formatTime(timestamp):
+	ymdhms = time.localtime(timestamp)[0:6]
+	return "{}-{:02}-{:02} {:02}:{:02}:{:02}".format(*ymdhms)
+
+def setTimeExif(fileName, verbose=0):
+	image = Image(fileName)
+	exifTime = image.getTimeCreated()
+	if exifTime == 0:
+		if verbose > 1:
+			print fileName, "doesn't have Exif DateTimeOriginal"
+		return False
+
+	fileInfo = os.stat(fileName)
+	if fileInfo.st_mtime in (exifTime, exifTime + 1):
+		if verbose > 2:
+			print fileName, "already has its mod time equal to the Exif time"
+		return False
+
+	if verbose > 0:
+		oldTime = formatTime(fileInfo.st_mtime)
+		newTime = formatTime(exifTime)
+		print fileName, "mod time set to {} (was {})".format(newTime, oldTime)
+
+	os.utime(fileName, (exifTime, exifTime))
+	return True
+
+def parseArgs():
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('imagePath', nargs='+')
+	parser.add_argument('--verbose', '-v', action='count', default=0)
+	args = parser.parse_args()
+	args.verbose += 1
+
+	for fileName in args.imagePath:
+		setTimeExif(fileName, args.verbose)
+
+if __name__ == '__main__':
+	parseArgs()
