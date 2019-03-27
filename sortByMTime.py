145806ebc2be8a7d01bbe5f83a7a397a80d4fcf6
Pius Fischer <pius@nightjuggler.com>
2019-03-27 02:29:26 -0700

    Script to rename image files so they're sorted by modification time

---
+++ sortByMTime.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import os
+import os.path
+import sys
+
+def main():
+	time_name_pairs = [(os.stat(name).st_mtime, name) for name in os.listdir('.')
+		if name[:4] == 'IMG_' and name[-4:] == '.JPG']
+	time_name_pairs.sort()
+
+	names = []
+	for i, (mtime, oldname) in enumerate(time_name_pairs, start=1):
+		newname = 'IMG_{:04d}.JPG'.format(i)
+		tmpname = 'tmp_' + newname
+		if os.path.exists(tmpname):
+			sys.exit('"{}" already exists!'.format(tmpname))
+		names.append((oldname, tmpname, newname))
+
+	for oldname, tmpname, newname in names:
+		print 'Renaming', oldname, 'to', tmpname
+		os.rename(oldname, tmpname)
+
+	for oldname, tmpname, newname in names:
+		if os.path.exists(newname):
+			sys.exit('"{}" already exists!'.format(newname))
+		print 'Renaming', tmpname, 'to', newname
+		os.rename(tmpname, newname)
+
+if __name__ == '__main__':
+	main()
