cac216d6d188db6f2218a8883ca0e7d6b1add7e3
nightjuggler <pius@nightjuggler.com>
2024-06-18 13:25:15 -0700

    Use 4x3 aspect ratio and change filename format to match iPhone

--- spec.py
+++ spec.py
@@ -2,22 +2,22 @@ title = ''
 date = ''
 sort_by_time = False
 time_adjust = 0
-width = 900
-height = 600
-thumb_width = 210
-thumb_height = 140
-thumb_cols = 4
-thumb_rows = 3
+width = 920
+height = 690
+thumb_width = 360
+thumb_height = 270
+thumb_cols = 2
+thumb_rows = 60
 
+skip = ()
 rotate_left = ()
 rotate_right = ()
-skip = ()
 
-filenameFormat = "DSC_%04u.JPG"
+filenameFormat = 'IMG_%04u.HEIC'
 
+skip = [filenameFormat % n for n in skip]
 rotate_left = [filenameFormat % n for n in rotate_left]
 rotate_right = [filenameFormat % n for n in rotate_right]
-skip = [filenameFormat % n for n in skip]
 
 crop = [
 ]
